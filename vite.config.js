bf8afe9f64a380d75e1c0f6e9e1bb7c6b7e26327
Ishrak <admin@ishrak.xyz>
2026-05-16 20:44:47 -0500

    Update Vite configuration to set allowedHosts to true

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-const allowedHosts = [
-  "hackmate.srv01.ishrak.xyz",
-  "admin.hackmate.srv01.ishrak.xyz",
-];
-
 export default defineConfig({
   plugins: [react()],
   server: {
     host: "0.0.0.0",
     port: 5173,
-    allowedHosts,
+    allowedHosts: true,
   },
   preview: {
     host: "0.0.0.0",
     port: 4173,
-    allowedHosts,
+    allowedHosts: true,
   },
 });
