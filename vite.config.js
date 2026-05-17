7a5d1d5794e9d76d1eafd99334b8d431e05ef3c9
Ishrak <admin@ishrak.xyz>
2026-05-16 20:39:28 -0500

    Update Vite configuration to specify host for server and preview

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,11 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: "0.0.0.0",
+    host: "hackmate.srv01.ishrak.xyz",
     port: 5173,
   },
   preview: {
-    host: "0.0.0.0",
+    host: "hackmate.srv01.ishrak.xyz",
     port: 4173,
   },
 });
