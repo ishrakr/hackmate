463c0951803a403f965b2b60e4740319b779870c
Ishrak <admin@ishrak.xyz>
2026-05-16 20:41:03 -0500

    Update Vite configuration to use 0.0.0.0 as host for server and preview

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,13 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: "hackmate.srv01.ishrak.xyz",
+    host: "0.0.0.0",
     port: 5173,
+    allowedHosts: ["hackmate.srv01.ishrak.xyz"],
   },
   preview: {
-    host: "hackmate.srv01.ishrak.xyz",
+    host: "0.0.0.0",
     port: 4173,
+    allowedHosts: ["hackmate.srv01.ishrak.xyz"],
   },
 });
